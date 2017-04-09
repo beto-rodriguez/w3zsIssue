10264bffcbafaf7bdac164fdc8e447d42117947c
Alberto Rodríguez <bto.rdz@hotmail.com>
2017-04-09 13:34:24 -0500

    Add project files.

---
+++ Testing.Winforms/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Testing.Winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Media.SolidColorBrush solidColorBrush1 = new System.Windows.Media.SolidColorBrush();
+            this.dopGraph = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // dopGraph
+            // 
+            this.dopGraph.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dopGraph.Hoverable = true;
+            this.dopGraph.Location = new System.Drawing.Point(0, 0);
+            this.dopGraph.Name = "dopGraph";
+            solidColorBrush1.Color = System.Windows.Media.Color.FromArgb(((byte)(30)), ((byte)(30)), ((byte)(30)), ((byte)(30)));
+            this.dopGraph.ScrollBarFill = solidColorBrush1;
+            this.dopGraph.ScrollHorizontalFrom = 0D;
+            this.dopGraph.ScrollHorizontalTo = 0D;
+            this.dopGraph.ScrollMode = LiveCharts.ScrollMode.None;
+            this.dopGraph.ScrollVerticalFrom = 0D;
+            this.dopGraph.ScrollVerticalTo = 0D;
+            this.dopGraph.Size = new System.Drawing.Size(538, 337);
+            this.dopGraph.TabIndex = 0;
+            this.dopGraph.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 337);
+            this.Controls.Add(this.dopGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart dopGraph;
+    }
+}
+
---
+++ Testing.Winforms/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System.Windows.Forms;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Geared;
+using LiveCharts.Geared.Geometries;
+using LiveCharts.Wpf;
+
+namespace Testing.Winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        public GearedValues<ObservablePoint> dopplerPt { get; set; }
+        public GearedValues<ObservablePoint> headingPt { get; set; }
+        public GearedValues<ObservablePoint> speedPt { get; set; }
+        public SeriesCollection dopplerPlots { get; set; }
+
+        public void Init()
+        {
+            GearedValues<ObservablePoint> dopplerPt = new GearedValues<ObservablePoint>();
+            GearedValues<ObservablePoint> headingPt = new GearedValues<ObservablePoint>();
+            GearedValues<ObservablePoint> speedPt = new GearedValues<ObservablePoint>();
+
+            System.Windows.Media.BrushConverter converter = new System.Windows.Media.BrushConverter();
+            var brush = (System.Windows.Media.Brush)converter.ConvertFromString("#000000");
+            if (brush.CanFreeze)
+            {
+                brush.Freeze();
+            }
+            Separator separator = new Separator();
+
+            dopplerPlots = new SeriesCollection
+            {
+                new GScatterSeries
+                {
+                    Title = "Doppler Shift",
+                    Values = dopplerPt,
+                    GearedPointGeometry = GearedGeometries.Diamond,
+                    MaxPointShapeDiameter =10,
+                    FontSize = 8,
+                    ScalesYAt = 0,
+                    Fill = brush
+                },
+                new GScatterSeries
+                {
+                    Title = "Heading",
+                    Values = headingPt,
+                    GearedPointGeometry = GearedGeometries.Diamond,
+                    MaxPointShapeDiameter = 8,
+                    FontSize = 8,
+                    ScalesYAt = 1
+                },
+                new GScatterSeries
+                {
+                    Title = "Speed",
+                    Values = speedPt,
+                    GearedPointGeometry = GearedGeometries.Diamond,
+                    MaxPointShapeDiameter = 8,
+                    FontSize = 8,
+                    ScalesYAt =2
+                }
+            };
+
+            dopGraph.AxisY.Add(new Axis
+            {
+                Position = AxisPosition.LeftBottom,
+                Title = "Hz",
+                Foreground = brush,
+                FontWeight = System.Windows.FontWeights.Bold,
+                FontSize = 15,
+                //LabelFormatter = value => ((Math.Abs(rateDopChange) <= 0.01) && (dopplerDataList.Count < 100)) ? value.ToString("F2") :
+                //    ((Math.Abs(rateDopChange) > 0.01) && (Math.Abs(rateDopChange) <= 0.02) && (dopplerDataList.Count < 50)) ? value.ToString("F2") :
+                //        ((Math.Abs(rateDopChange) > 0.02) && (Math.Abs(rateDopChange) <= 0.03) && (dopplerDataList.Count < 42)) ? value.ToString("F2") :
+                //            ((Math.Abs(rateDopChange) > 0.03) && (Math.Abs(rateDopChange) <= 0.04) && (dopplerDataList.Count < 34)) ? value.ToString("F2") :
+                //                ((Math.Abs(rateDopChange) > 0.04) && (Math.Abs(rateDopChange) <= 0.05) && (dopplerDataList.Count < 25)) ? value.ToString("F2") :
+                //                    ((Math.Abs(rateDopChange) > 0.05) && (Math.Abs(rateDopChange) <= 0.1) && (dopplerDataList.Count < 20)) ? value.ToString("F2") :
+                //                        ((Math.Abs(rateDopChange) > 0.1) && (Math.Abs(rateDopChange) <= 0.5) && (dopplerDataList.Count < 20)) ? value.ToString("F2") :
+                //                            ((Math.Abs(rateDopChange) > 0.5) && (Math.Abs(rateDopChange) <= 1.0) && (dopplerDataList.Count < 10)) ? value.ToString("F2") :
+                //                                Math.Round(value).ToString(),
+                Separator = new Separator
+                {
+                    Stroke = brush
+                },
+            });
+
+            dopGraph.AxisY.Add(new Axis
+            {
+                Position = AxisPosition.RightTop,
+                Title = "Degrees  (0 - 360)",
+                ShowLabels = false,
+                MaxValue = 370,
+                MinValue = 0,
+                Foreground = brush,
+                FontWeight = System.Windows.FontWeights.Bold,
+                FontSize = 15,
+                Separator = new Separator
+                {
+                    IsEnabled = false
+                },
+            });
+            dopGraph.AxisY.Add(new Axis
+            {
+                Position = AxisPosition.RightTop,
+                Title = "km/h",
+                MaxValue = 1300,
+                MinValue = 0,
+                Foreground = brush,
+                FontWeight = System.Windows.FontWeights.Bold,
+                FontSize = 15,
+                Separator = new Separator
+                {
+                    IsEnabled = false
+                }
+            });
+            dopGraph.AxisX.Add(new Axis
+            {
+                Position = AxisPosition.LeftBottom,
+                Title = "Seconds",
+                MinValue = 0,
+                MinRange = 10,
+                Foreground = brush,
+                FontWeight = System.Windows.FontWeights.Bold,
+                FontSize = 15,
+                Separator = new Separator
+                {
+                    Stroke = brush
+                }
+            });
+
+            dopGraph.Hoverable = false;
+            dopGraph.DataTooltip = null;
+            dopGraph.DisableAnimations = false;
+            dopGraph.Series = dopplerPlots;
+            dopGraph.LegendLocation = LegendLocation.Top;
+            //not necessary
+            //dopGraph.ForeColor = Colors.Black;
+
+            foreach (var series in dopplerPlots)
+            {
+                if (series.Title == "Doppler Shift")
+                {
+                    series.Values.Add(new ObservablePoint(9, 9));
+                }
+                else if (series.Title == "Heading")
+                {
+                    series.Values.Add(new ObservablePoint(8, 8));
+                }
+                else if (series.Title == "Speed")
+                {
+                    series.Values.Add(new ObservablePoint(7, 7));
+                }
+            }
+        }
+    }
+}
